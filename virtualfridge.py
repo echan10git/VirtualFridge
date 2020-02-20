52ab583a3ed94b6326d93ef9f0d39acbef29ad30
echan10git <61261697+echan10git@users.noreply.github.com>
2020-02-20 16:31:01 -0500

    Add files via upload

---
+++ virtualfridge.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def landing_page():
+    return render_template('landing_page.html', title='Landing Page')
+
+@app.route("/home")
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route("/myfridge")
+def myfridge():
+    return render_template('myfridge.html', title='My Fridge')
+
+@app.route("/search")
+def search():
+    return render_template('search.html', title='Search')
+
+@app.route("/submit")
+def submit():
+    return render_template('submit.html', title='Submit')
+
+@app.route("/popular")
+def popular():
+    return render_template('popular.html', title='Popular')
+
+@app.route("/help")
+def help():
+    return render_template('help.html', title='Help')
+
