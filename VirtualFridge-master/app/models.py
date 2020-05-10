a93493288a6af759a3728e95ab5bfbccf87c37d3
fahimiah34 <59512314+fahimiah34@users.noreply.github.com>
2020-05-10 18:04:51 -0400

    Add files via upload

---
+++ VirtualFridge-master/app/models.py
@@ -0,0 +1,30 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login, db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
+    fridge = db.relationship('Fridge', lazy = 'dynamic')
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class Fridge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ingredient = db.Column(db.String(140), index = True)
+    quantity = db.Column(db.Integer, index = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def _repr_(self):
+        return '<Fridge {}>'.format(self.ingredient)
---
+++ VirtualFridge-master/app/routes.py
@@ -0,0 +1,71 @@
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+
+@app.route("/")
+def landing_page():
+    return render_template('landing_page.html', title='Landing Page')
+
+@app.route("/home")
+@login_required
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route("/myfridge")
+def myfridge():
+    return render_template('myfridge.html', title='My Fridge')
+
+@app.route("/search")
+def search():
+    return render_template('search.html', title='Search')
+
+@app.route("/submit")
+def submit():
+    return render_template('submit.html', title='Submit')
+
+@app.route("/popular")
+def popular():
+    return render_template('popular.html', title='Popular')
+
+@app.route("/help")
+def help():
+    return render_template('help.html', title='Help')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/registration", methods=['GET','POST'])
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration complete!')
+        return redirect(url_for('login'))
+    return render_template('registration.html', title="Registration", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
