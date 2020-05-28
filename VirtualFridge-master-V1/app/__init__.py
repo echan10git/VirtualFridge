ade0b1551b9bf74fcec4eb7344b29295927d506a
echan10git <61261697+echan10git@users.noreply.github.com>
2020-05-27 20:31:35 -0400

    Add files via upload

---
+++ VirtualFridge-master-V1/app/__init__.py
@@ -0,0 +1,75 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
+from flask_mail import Mail
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from elasticsearch import Elasticsearch
+
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please log in to access this page.'
+mail = Mail()
+
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if current_app.config['MAIL_USERNAME'] or current_app.config['MAIL_PASSWORD']:
+                auth = (current_app.config['MAIL_USERNAME'],
+                        current_app.config['MAIL_PASSWORD'])
+            secure = None
+            if current_app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/virtualfridge.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Virtualfridge startup')
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
+    return app
+
+from app import models
---
+++ VirtualFridge-master-V1/app/main/forms.py
@@ -0,0 +1,51 @@
+from flask import request
+from flask_wtf import FlaskForm
+from flask_babel import lazy_gettext as _1
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from app.models import User, Ingredients
+# from app import mall
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators = [Length(min=0, max = 140)])
+    submit = SubmitField('Submit')
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+class SearchForm(FlaskForm):
+    q = StringField('Search', validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
+
+class RecipeForm(FlaskForm):
+    ingredient = StringField('Ingredient', validators=[DataRequired()])
+    steps = TextAreaField('Steps', validators = [Length(min=0, max = 140)])
+    add = SubmitField('')
+    submit = SubmitField('Done!')
+
+class AddIngredientsForm(FlaskForm):
+    ingredient = StringField('Ingredient', validators=[DataRequired()])
+    submit = SubmitField('Add')
+
+    def validate_ingredient(self, knownIngredients):
+        ingredients = Ingredients.query.filter_by(knownIngredients=knownIngredients.data).first()
+        if ingredients is not None:
+            raise ValidationError('Already added!')
+
+
