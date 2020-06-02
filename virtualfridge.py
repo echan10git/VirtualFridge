6aab09e5ac7d388a92e7b2df90220edea4290c3b
echan10git <61261697+echan10git@users.noreply.github.com>
2020-06-02 16:05:18 -0400

    Add files via upload

--- virtualfridge.py
+++ virtualfridge.py
@@ -1,5 +1,7 @@
-from app import app, db
-from app.models import User, Fridge
+from app import create_app, db
+from app.models import User, Fridges, Recipes, RecipeIngredients, Ingredients, Link
+
+app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
