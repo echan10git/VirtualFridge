27887cb2a40119d1a0a30e7c4ecd607921d7779d
echan10git <61261697+echan10git@users.noreply.github.com>
2020-06-02 16:06:59 -0400

    Add files via upload

---
+++ app/main/routes.py
@@ -0,0 +1,132 @@
+from flask import render_template, flash, redirect, url_for, request, g, current_app
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import db
+from app.main.forms import EditProfileForm, EmptyForm, SearchForm, RecipeForm, AddIngredientsForm
+from app.models import User, Fridges, Recipes, RecipeIngredients, Ingredients, Link, Linktwo
+from app.main import bp
+
+@bp.before_app_request
+def before_request():
+    db.session.commit()
+    g.search_form = SearchForm()
+
+@bp.route("/")
+def landing_page():
+    return render_template('landing_page.html', title='Landing Page')
+
+@bp.route("/home")
+@login_required
+def home():
+    return render_template('home.html', title='Home')
+
+@bp.route("/recipesearch")
+def recipe_search():
+    return render_template('recipesearch.html', title='Recipe Search')
+
+@bp.route("/myfridge")
+def myfridge():
+    return render_template('myfridge.html', title='My Fridge')
+
+@bp.route("/submit", methods=['GET', 'POST'])
+@login_required
+def submit():
+    form = RecipeForm()
+    if form.validate_on_submit():
+            recipe = Recipes(title = form.title.data, body = form.step.data, diet = form.hasDiet.data, spiceLevel = form.spiceLevel.data, user_id = current_user.id)
+            return redirect(url_for('main.recipeingredientssearch', recipes=recipe))
+    return render_template('recipe.html', title='Add a Recipe', form=form)
+
+@bp.route("/popular")
+def popular():
+    return render_template('popular.html', title='Popular')
+
+@bp.route("/help")
+def help():
+    return render_template('help.html', title='Help')
+
+@bp.route('/edit_profile', methods = ['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.diet = form.diet.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Changes successfully made!')
+        return redirect(url_for('main.user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+        form.diet.data = current_user.diet
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@bp.route('/addingredientstest', methods=['GET', 'POST'])
+def addingredientstest():
+    form = AddIngredientsForm()
+    if form.validate_on_submit():
+        addition = Ingredients(form.ingredient.data)
+        db.session.add(addition)
+        db.session.commit()
+        return redirect(url_for('main.home'))
+    return render_template('addingredientstest.html', title = 'Add Ingredients', form=form)
+
+@bp.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+@bp.route('/search')
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.home'))
+    page = request.args.get('page', 1, type = int)
+    recipe, total = Recipes.search(g.search_form.q.data, page, current_app.config['RECIPES_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['RECIPES_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title='Search', recipe=recipe,
+                           next_url=next_url, prev_url=prev_url)
+
+@bp.route('/addingredients')
+def addyouringredients():
+    if not g.search_form.validate():
+            return redirect(url_for('main.home'))
+    page = request.args.get('page', 1, type = int)
+    ingredient, total = Ingredients.search(g.search_form.q.data, page, current_app.config['INGREDIENTS_PER_PAGE'])
+    next_url = url_for('main.addingredients', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['INGREDIENTS_PER_PAGE'] else None
+    prev_url = url_for('main.addingredients', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('addingredients.html', title='Add Ingredients', ingredient=ingredient,
+                        next_url=next_url, prev_url=prev_url)
+
+@bp.route('/added')
+def add(ingredient):
+    fridgenew = Fridges(quantity = 1)
+    fridgenew.currentingredients.append(ingredient)
+    current_user.fridge.append(fridgenew)
+    db.session.commit()
+    return redirect(url_for('main.myfridge'))
+
+@bp.route('/recipeingredientssearch')
+def recipeingredientssearch():
+    recipes = request.args.get('recipes', None)
+    return render_template('recipeingredientssearch.html', title='Add Ingredients to Your Recipe', recipes=recipes)
+
+@bp.route('/addingredientstorecipe')
+def addingredientstorecipe():
+    recipes = request.args.get('recipes', None)
+    if not g.search_form.validate():
+            return redirect(url_for('main.home'))
+    page = request.args.get('page', 1, type = int)
+    ingredient, total = Ingredients.search(g.search_form.q.data, page, current_app.config['INGREDIENTS_PER_PAGE'])
+    next_url = url_for('main.addingredientstorecipe', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['INGREDIENTS_PER_PAGE'] else None
+    prev_url = url_for('main.addingredientstorecipe', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('addingredientstorecipe.html', title='Add Ingredients', ingredient=ingredient,
+                        next_url=next_url, prev_url=prev_url, recipes = recipes)
--- app/models.py
+++ app/models.py
@@ -1,13 +1,64 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login, db
+from hashlib import md5
+from app.search import add_to_index, remove_from_index, query_index
+
+
+class SearchableMixin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return cls.query.filter_by(id=0), 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        return cls.query.filter(cls.id.in_(ids)).order_by(
+            db.case(when, value=cls.id)), total
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
+
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
+    about_me = db.Column(db.String(140))
+    diet = db.Column(db.String(64))
     password_hash = db.Column(db.String(128))
-    fridge = db.relationship('Fridge', lazy = 'dynamic')
+    fridge = db.relationship('Fridges', backref='author', lazy = 'dynamic')
+    recipe = db.relationship('Recipes', backref='author', lazy = 'dynamic')
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -17,14 +68,91 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def get_reset_password_token(self, expires_in = 600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-class Fridge(db.Model):
+class Fridges(db.Model):
+    __tablename__ = 'fridges'
     id = db.Column(db.Integer, primary_key=True)
-    ingredient = db.Column(db.String(140), index = True)
+    currentingredients = db.relationship('Ingredients', secondary='linktwo')
     quantity = db.Column(db.Integer, index = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def addingredients(self, ingredients):
+        if not self.is_using(ingredients):
+            self.currentingredients.append(ingredients)
+    def removeingredients(self, ingredients):
+        if self.is_using(ingredients):
+            self.currentingredients.remove(ingredients)
+    def is_using(self, ingredients):
+        return self.currentingredients.filter(link.c.ingredients_id == ingredients.id).count() > 0
     def _repr_(self):
-        return '<Fridge {}>'.format(self.ingredient)
+        return '<Fridges {}>'.format(self.ingredient)
+
+class Recipes(SearchableMixin, db.Model):
+    __searchable__ = ['title']
+    __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(140))
+    body = db.Column(db.Text)
+    diet = db.Column(db.String(140))
+    spiceLevel = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    RecipeIngredient = db.relationship('RecipeIngredients', backref='recipeorigin', lazy = 'dynamic')
+    def showingredients(self):
+        return self.query.join(link, (self.id == RecipeIngredients.recipe_id)).filter(
+            Link.c.recipeingredients_id == RecipeIngredients.recipe_id).order_by(
+            Recipe.likes.desc())
+
+class RecipeIngredients(db.Model):
+    __tablename__ = 'recipeingredients'
+    id = db.Column(db.Integer, primary_key = True)
+    quantity = db.Column(db.Numeric)
+    units = db.Column(db.String(140))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+    usedingredients = db.relationship('Ingredients', secondary='link')
+    def addingredients(self, ingredients):
+        if not self.is_using(ingredients):
+            self.usedingredients.append(ingredients)
+    def removeingredients(self, ingredients):
+        if self.is_using(ingredients):
+            self.usedingredients.remove(ingredients)
+    def is_using(self, ingredients):
+        return self.usedingredients.filter(link.c.ingredients_id == ingredients.id).count() > 0
+
+class Ingredients(SearchableMixin, db.Model):
+    __searchable__ = ['knownIngredients']
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key = True)
+    knownIngredients = db.Column(db.String(64), index=True, unique=True)
+    recipeingredient = db.relationship('RecipeIngredients', secondary='link')
+    fridgeingredients = db.relationship('Fridges', secondary='linktwo')
+
+    def __init__(self, inputingredient):
+        self.knownIngredients = inputingredient
+
+class Link(db.Model):
+    __tablename__ = 'link'
+    recipeingredients_id = db.Column(db.Integer, db.ForeignKey('recipeingredients.id'), primary_key = True)
+    ingredients_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), primary_key = True)
+
+class Linktwo(db.Model):
+    __tablename__ = 'linktwo'
+    fridges_id = db.Column(db.Integer, db.ForeignKey('fridges.id'), primary_key = True)
+    ingredients_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), primary_key = True)
+
+
